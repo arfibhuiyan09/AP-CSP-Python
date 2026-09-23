b57c8a0eb4c4d7ada7a10f0a5f9b7ccb7d3a22f6
arfibhuiyan09 <144164033+arfibhuiyan09@users.noreply.github.com>
2026-09-23 20:20:31 +0000

    09/23/26

--- Python/Fridays/09-24-26/receipt_total.py
+++ Python/Fridays/09-24-26/receipt_total.py
@@ -7,11 +7,13 @@ tax_rate = int(input("Tax Rate: "))
 if (100 <= subtotal <= 500):
     if (10 <= discount <= 20):
         if (3 <= tax_rate <= 9):
-            total = subtotal - discount * (tax_rate/100)
-            print(total)
+           discounted_subtotal = subtotal - discount
+           tax_amount = discounted_subtotal * (tax_rate / 100)
+           total = discounted_subtotal + tax_amount
+           print(total)
         else:
             sys.exit()
     else:
-        sys.exit
+        sys.exit()
 else:
     sys.exit()
